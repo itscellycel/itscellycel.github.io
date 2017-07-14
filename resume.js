7cf6253acc95d1c33a45f6e56ef3ce8c733aa532
itscellycel <marcell.urbina@gmail.com>
2017-07-14 14:58:30 -0700

    created contact page still a work in progress

--- resume.js
+++ resume.js
@@ -1,13 +1,12 @@
-/*$(window).scroll(function() {
-  if ($(this).scrollTop()> $(window).height()*.5) {
-    $("div.skills").css("opacity", 1);
-}
-  if ($(this).scrollTop()> $(window).height()) {
-    $("div.work").css("opacity", 1);
-    $("div.volunteer").css("opacity", 1);
-  }
-  if ($(this).scrollTop()> $(window).height()*1.5) {
-    $("div.degrees").css("opacity", 1);
-
-  }
-*/
+// $(window).scroll(function() {
+//   if ($(this).scrollTop()> $(window).height()*.5) {
+//     $("div.skills").css("opacity", 1);
+// }
+//   if ($(this).scrollTop()> $(window).height()) {
+//     $("div.work").css("opacity", 1);
+//     $("div.volunteer").css("opacity", 1);
+//   }
+//   if ($(this).scrollTop()> $(window).height()*1.5) {
+//     $("div.degrees").css("opacity", 1);
+//
+//   }
