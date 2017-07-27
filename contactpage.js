92077bc3c50df05eb2632741ab8d9d257d15fa93
itscellycel <marcell.urbina@gmail.com>
2017-07-27 13:13:01 -0700

    contact

---
+++ contactpage.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function () {
+    submitContactForm();
+});
+
+
+
+function submitContactForm() {
+
+    $('#contact-form-submit').on("click",function (e) {
+        e.preventDefault();
+        var contactFirst = $('#contact-first').val();
+        var contactLast = $('#contact-last').val();
+        var contactEmail = $('#contact-email').val();
+        var contactPhone = $('#contact-phone').val();
+        var contactMessage = $('#contact-message').val();
+        // data validation code here
+        var url = "//docs.google.com/forms/d/e/1FAIpQLScq4L1Ri9teYVCUM88wYZWStatmKvAe49VLimhOlgiD3mxgYQ/formResponse";
+        var data = {
+            'entry.2005620554': contactFirst,
+            'entry.834360636': contactLast,
+            'entry.1045781291': contactEmail,
+            'entry.1166974658': contactPhone,
+            'entry.839337160': contactMessage
+        };
+        $.ajax({
+            type: "POST",
+            url: url,
+            dataType: "json",
+            data: data,
+            'Content-Type':'application/x-www-form-urlencoded',
+            statusCode: {
+                0: function () {
+                    // TEMP: CORS ERROR is returning a 401 but data is still getting passed through. Always return a success
+                    // window.location.href = "../volunteer";
+                    console.log("djsfnsdljnfdslnflsjdf")
+                },
+                200: function () {
+                    window.location.href = "../success";
+                }
+            }
+        });
+    });
+}
